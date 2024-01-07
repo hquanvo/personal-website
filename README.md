361de3778fb82ba42f176115fd7a530b90f28dc8
Norman Vo <92652998+hquanvo@users.noreply.github.com>
2024-01-06 21:22:01 -0800

    Add basic structure of skills section

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# React + Vite
+Todo list:
+# General
+    - Add "Project" Section
+    - Add "Footer"
+    - Make the website mobile compatible (with resizing screens and such)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Navigation Bar
+    - Style anchors to make it more visible that they're clickable
 
-Currently, two official plugins are available:
+# About me
+    - Add a photo
+    - Write a description about myself
+    - Probably resize the section so that it takes up the screen
+    - On smaller screen size, the section should be shown the following from top to bottom: About me, photo, description
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Skills
+    - Touch up on the CSS used here
+    - Also resize the section so that it takes up the screen
+    - On smaller screen size, rearrange the 3 sections so that it's shown consecutively one after another instead of arranging all three size by size
+
+# Project
+    - Create a 3x3 grid
+    - Link some projects github into the some of the grid slot. If live demo is possible, link the demo as well
+    - Unusued slots can have a coming soon message
+
+# Footer
+    - Hook up github and linkedin
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,45 @@
 function Skills() {
     return (
-        <div> Skills </div>
+        <div class = "flex flex-col min-h-[420px] items-center p-8" id="skills"> 
+            <div class = "w-full text-center text-4xl font-semibold" >Skills</div>
+            <div class = "flex w-2/3 py-6 justify-between text-2xl">
+                <div class = "bg-neutral-100 min-w-60 flex flex-col items-center p-10 shadow-md shadow-blue-800/70 hover:shadow-lg hover:shadow-blue-900">
+                    <img class = "size-24 flex justify-center" src = "/binary-code.png"></img>
+                    <div class = "py-4 font-semibold">Languages</div>
+                    <ul class = "list-outside list-disc text-xl">
+                        <li >Java</li>
+                        <li class = "mt-3">JavaScript</li>
+                        <li class = "mt-3">HTML/CSS</li>
+                        <li class = "mt-3">C/C++</li>
+                        <li class = "mt-3">R</li>
+                        <li class = "mt-3">SQL</li>
+                    </ul>
+                </div>
+                <div class = "bg-neutral-100 min-w-60 flex flex-col items-center p-10 shadow-md shadow-blue-800/70 hover:shadow-lg hover:shadow-blue-900">
+                    <img class = "size-24" src = "/framework.png"></img>
+                    <div class = "py-4 font-semibold">Frameworks/Libraries</div>
+                    <ul class = "list-outside list-disc text-xl">
+                        <li >JUnit</li>
+                        <li class = "mt-3">React.js</li>
+                        <li class = "mt-3">Node.js</li>
+                        <li class = "mt-3">Tailwind CSS</li>
+                    </ul>
+                </div>
+                <div class = "bg-neutral-100 min-w-60 flex flex-col items-center p-10 shadow-md shadow-blue-800/70 hover:shadow-lg hover:shadow-blue-900">
+                    <img class = "size-24" src = "/tool-box.png"></img>
+                    <div class = "py-4 font-semibold">Development Tools</div>
+                    <ul class = "list-outside list-disc text-xl">
+                        <li >Visual Studio Code</li>
+                        <li class = "mt-3">Visual Studio</li>
+                        <li class = "mt-3">PyCharm</li>
+                        <li class = "mt-3">IntelliJ</li>
+                        <li class = "mt-3">JupyterHub</li>
+                        <li class = "mt-3">Git</li>
+                        <li class = "mt-3">GitHub</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
